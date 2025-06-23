8a5737ea3817109bc04ee9757247d9754604430d
ronitkongara <ronitkongara@gmail.com>
2025-06-23 13:54:09 +0530

    Updates routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,14 +3,20 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import StartPage from './pages/StartPage';
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
+import ListingsPage from './pages/ListingsPage';  
+import CreateListings from './pages/CreateListings';
 
 function App() {
   return (
-    <Routes>
+    <Router>
+      <Routes>
         <Route path="/" element={<StartPage />} />
         <Route path="/signin" element={<SignIn />} />
         <Route path="/signup" element={<SignUp />} />
-    </Routes>
+        <Route path="/listingspage" element={<ListingsPage />} />
+        <Route path="/createlistings" element={<CreateListings />} />
+      </Routes>
+    </Router>
   );
 }
 
