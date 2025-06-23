e1fdba9330cf30f0d5a5b476caf54f1742b4adaf
ronitkongara <ronitkongara@gmail.com>
2025-06-23 20:49:25 +0530

    arrow

--- frontend/src/pages/CreateListings.jsx
+++ frontend/src/pages/CreateListings.jsx
@@ -128,7 +128,7 @@ function CreateListing({ onCreate }){
             <div className="create-listing-container">
                 <button onClick={handleBackToStart} className="back-to-start-btn">
                     <svg width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor">
-                        <path d="M12 19-7-7 7-7"/>
+                        <path d="m12 19-7-7 7-7"/>
                         <path d="M19 12H5"/>
                     </svg>
                     Back to Home
